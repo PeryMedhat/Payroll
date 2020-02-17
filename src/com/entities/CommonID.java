f318b49d8cbb60d5d6a5b585e8396b9b627de412
PeryMedhat <prihan1994@gmail.com>
2020-02-17 14:49:34 +0200

    first

---
+++ src/com/entities/CommonID.java
@@ -0,0 +1,37 @@
+package com.entities;
+
+import java.util.Date;
+
+public class CommonID {
+
+	
+	Date startDate ;
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	Date endDate;
+	String code;
+	String name;
+	
+}
---
+++ src/com/entities/EmployeeStruct_Parent.java
@@ -0,0 +1,7 @@
+package com.entities;
+
+public class EmployeeStruct_Parent extends CommonID {
+	
+	
+
+}
