2720a785d5eb428ea059efb2ee2d1e59edd24b60
AyaMoawad <ayamoawad@gmail.com>
2020-02-17 17:06:50 +0200

    added a TestDB servlet

--- Java/com/services/servlet.java
+++ Java/com/services/servlet.java
@@ -1,15 +1,17 @@
 package com.services;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class servlet
- */
+
 @WebServlet("/servlet")
 public class servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -26,9 +28,26 @@ public class servlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		// setup connection variables
+				String user = "springstudent";
+				String pass = "springstudent";
+				String jdbcUrl = "jdbc:mysql://localhost:3306/payroll-schema?useSSL=false";
+				//String driver = "com.mysql.jdbc.Driver";
+				
+				//get connection to database
+				try {
+					PrintWriter out = response.getWriter();
+					out.println("Connecting to DB .."+jdbcUrl);
+					Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+					out.println("SUCCESS!!");
+					myConn.close();
+					
+				}
+				catch(Exception exc) {
+					exc.printStackTrace();
+					
+				}
+				}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
