5b242cfc9d66805ad19493a70bf53805b03a4392
PeryMedhat <prihan1994@gmail.com>
2020-03-13 15:30:49 +0200

    adding the frontend for creation the employee structure master data

---
+++ WebContent/resources/JS/main.js
@@ -0,0 +1,116 @@
+(function($) {
+	
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	$('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
+  });
+
+
+})(jQuery);
+var x = 0;
+var empObjectsArray = Array();
+
+	"use strict";
+
+function yesnoCheck() {
+    if (document.getElementById('yesCheck').checked) {
+		document.getElementById('buttonSubmit').innerHTML = 'Next';
+	
+    } 
+	if (document.getElementById('noCheck').checked) {
+		document.getElementById('buttonSubmit').innerHTML = 'Submit';
+
+	}
+}
+
+function NextOrSubmit(){
+	// in case of the next condition
+
+	if (document.getElementById('yesCheck').checked) {    	
+		var code =document.getElementById("empstruct_code");
+		var codeValue = code.value;
+
+		var name =document.getElementById("empstruct_name");
+		var nameValue = name.value;
+
+
+		var startDate =document.getElementById("start_date");
+		var startDateValue = startDate.value;
+
+
+		var endDate =document.getElementById("end_date");
+		var endDateValue = endDate.value;
+		const data = JSON.parse(localStorage.getItem('items'))
+		if(data === null){
+			addEmpObject(false,"",true,codeValue,nameValue,startDateValue,endDateValue);
+		}
+		else{
+			var lastParent = empObjectsArray[empObjectsArray.length - 1]
+			var lastParentValue = lastParent.code;
+			addEmpObject(true,lastParentValue	,true,codeValue,nameValue,startDateValue,endDateValue);
+		}
+		
+		
+	}
+ // in case of submission condition "we reached a child"
+	if (document.getElementById('noCheck').checked) {
+		var code =document.getElementById("empstruct_code");
+		var codeValue = code.value;
+
+		var name =document.getElementById("empstruct_name");
+		var nameValue = name.value;
+
+
+		var startDate =document.getElementById("start_date");
+		var startDateValue = startDate.value;
+
+
+		var endDate =document.getElementById("end_date");
+		var endDateValue = endDate.value;
+		var lastParent = empObjectsArray[empObjectsArray.length - 1]
+		var lastParentValue = lastParent.code;
+		addEmpObject(true,lastParentValue,false,codeValue,nameValue,startDateValue,endDateValue);
+		//send to XHR
+		var x = 0;
+		 empObjectsArray = Array();
+		 localStorage.removeItem('items');
+		 console.log(empObjectsArray);
+	}
+}
+
+function addEmpObject(hasParent,parentCode,hasChild,code ,name ,startDate,endDate){
+var code =code;
+var name = name;
+var startDate = startDate;
+var endDate = endDate;
+	var empObject = {
+		"code" : code,
+		"name" :name,
+		"startDate" :startDate,
+		"endDate":endDate,
+		"hasParent":hasParent,
+		"parentCode":parentCode,
+		"hasChild":hasChild	
+		
+}
+empObjectsArray[x] = empObject;
+localStorage.setItem('items', JSON.stringify(empObjectsArray))
+	x++;
+document.getElementById("empstruct_code").value="";
+document.getElementById("empstruct_name").value="";
+document.getElementById("start_date").value="";
+document.getElementById("end_date").value="";
+console.log(empObjectsArray);
+}
+
+
